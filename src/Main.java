2f5438790c9b5517fa1eefa483a67a03a51b85e8
Yang Liu <myly10@gmail.com>
2014-12-08 10:50:56 +0800

    Bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
-import java.util.ArrayDeque;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Main{
 	static final int MAX_THREADS=Runtime.getRuntime().availableProcessors();
@@ -8,19 +8,21 @@ public class Main{
 	public static void main(String[] args) throws IOException{
 		File in=new File(args[0]), out=new File(args[2]), qf=new File(args[1]);
 		Database[] db=dbReader(new BufferedInputStream(new FileInputStream(in)));
+		db.clone();
 	}
 
 	public static Database[] dbReader(BufferedInputStream r) throws IOException{
 		Scanner scn=new Scanner(r);
-		ArrayDeque<Database> db=new ArrayDeque<Database>();
+		Vector<Database> db=new Vector<Database>();
 		String t;
 		while (true){
-			scn.nextByte();
 			t=scn.nextLine();
-			if (t.equals("EOF")) break;
-			db.push(new Database(t, scn.nextLine()));
+			if (t.equals(">EOF")) break;
+			db.add(new Database(t.substring(1), scn.nextLine()));
 		}
 		r.close();
-		return (Database[])(db.toArray());
+		Database[] dt=new Database[db.size()];
+		for (int i=0;i!=db.size();++i) dt[i]=db.elementAt(i);
+		return dt;
 	}
 }
